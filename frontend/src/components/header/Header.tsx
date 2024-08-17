829ed3dad403bd6d7d5e1cfe9f65578ff7474c4a
SashkaAlkashka <113834536+SashkaAlkashka@users.noreply.github.com>
2024-08-17 15:58:08 +0300

    create routing

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,10 +1,32 @@
 import { FC } from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header: FC = () => {
+  const isAuth = false
   return (
-    <div>
-      HEADER
-    </div>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+            <Link to='/'> Логотип </Link>
+            {isAuth && (
+                <Nav className="me-auto">
+                    <NavLink className="ms-3" to={'/'}>Home</NavLink>
+                    <NavLink className="ms-3" to={'#'}>Trainer</NavLink>
+                </Nav>
+            )}
+
+            {
+                isAuth ? (
+                    <NavLink to={'#'}>Log out</NavLink>
+                ) : (
+                    <Link to={'auth'}>Log in/Sigh in</Link>
+                )
+            }
+
+        </Container>
+    </Navbar>
   )
 }
 
